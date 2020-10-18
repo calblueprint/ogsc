e98902af68690fe0e9edfadd8c349e3c67984f83
Joy <joydong@berkeley.edu>
2020-10-17 22:00:07 -0700

    WIP playerprofile next.js page (from Saturday worksession)

---
+++ pages/playerprofile.tsx
@@ -0,0 +1,17 @@
+const PlayerProfile: React.FunctionComponent = () => (
+  // add a padding to this box
+  <div className="flex ">
+    <div className="header flex">
+      {/* image */}
+      <image className="rounded-full h-16 w-16 flex items-center justify-center bg-gray-400">
+        a
+      </image>
+      {/* player name */}
+      {/* player number?? */}
+    </div>
+    {/* grid with buttons and each button will change the info shown on the screen */}
+    {/* title of each section */}
+  </div>
+);
+
+export default PlayerProfile;
