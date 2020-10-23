4a50305b7c342f75d8a3d0364db84575bd769184
Christopher <christophergrey@berkeley.edu>
2020-10-23 01:51:56 -0700

    finished icons + textField

---
+++ components/add_Note.tsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line camelcase
+const add_Note: React.FunctionComponent = () => (
+  <div className="mt-4">
+    <svg
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 29.25 18.25"
+    >
+      <defs>
+        <style>.cls-1{}</style>
+      </defs>
+      <path
+        className="cls-1"
+        d="M16.72.5H2.27A1.77 1.77 0 00.5 2.26v10.26a.49.49 0 000 .12.88.88 0 00.26.74l4.85 4.86a.93.93 0 00.64.26h10.47a1.77 1.77 0 001.78-1.76V2.26A1.77 1.77 0 0016.72.5zM5.6 15.66l-2.22-2.22H5.6zm11 1H7.45v-3.3a1.77 1.77 0 00-1.78-1.76H2.35V2.34h14.3zM29 3.25h-2.25V1a.75.75 0 00-1.5 0v2.25H23a.75.75 0 000 1.5h2.25V7a.75.75 0 001.5 0V4.75H29a.75.75 0 000-1.5z"
+        transform="translate(-.5 -.25)"
+      />
+    </svg>
+  </div>
+);
+
+// eslint-disable-next-line camelcase
+export default add_Note;
