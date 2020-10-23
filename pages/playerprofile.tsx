a27c91be5c35f359c980bab6fd9f22e47ed6c6b0
Joy <joydong@berkeley.edu>
2020-10-22 23:31:35 -0700

    Player profile navbar outline

--- pages/playerprofile.tsx
+++ pages/playerprofile.tsx
@@ -1,16 +1,55 @@
-const PlayerProfile: React.FunctionComponent = () => (
-  // add a padding to this box
-  <div className="flex ">
-    <div className="header flex">
-      {/* image */}
-      <image className="rounded-full h-16 w-16 flex items-center justify-center bg-gray-400">
-        a
-      </image>
-      {/* player name */}
-      {/* player number?? */}
+import { useState } from "react";
+
+const PlayerNavBar: React.FunctionComponent = () => {
+  const [title, setTitle] = useState("Overview");
+  const Button = (category: string): unknown => {
+    return (
+      <button
+        type="button"
+        className={
+          title === category
+            ? "bg-gray-300 py-3 px-8 rounded-full"
+            : "py-3 px-8 rounded-full"
+        }
+        onClick={() => {
+          setTitle(category);
+        }}
+      >
+        {category}
+      </button>
+    );
+  };
+  return (
+    <div>
+      <div className="flex flex-row justify-between text-xs text-center">
+        {Button("Overview")}
+        {Button("Engagement")}
+        {Button("Academic Performance")}
+        {Button("Attendance")}
+        {Button("Physical Wellness")}
+        {Button("Highlights")}
+      </div>
+      <div className="mt-12 mb-10 text-xl">{title}</div>
     </div>
-    {/* grid with buttons and each button will change the info shown on the screen */}
-    {/* title of each section */}
+  );
+};
+
+const PlayerProfile: React.FunctionComponent = () => (
+  <div className="flex mt-20 flex-wrap space-y-6 flex-col mx-16">
+    <div className="header flex">
+      <div className="picture flex mr-10">
+        <img
+          src="/reference to pic"
+          alt="..."
+          className="shadow rounded-full max-w-full align-middle border-none w-24 h-24"
+        />
+      </div>
+      <div className="player-info grid grid-rows-2">
+        <p className="pt-6 text-xl">Player Name</p>
+        <p className="pt-2 text-xs">Player Number</p>
+      </div>
+    </div>
+    {PlayerNavBar({})}
   </div>
 );
 
