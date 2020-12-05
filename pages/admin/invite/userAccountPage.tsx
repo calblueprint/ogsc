736cadbdf668a5178ee9a0466b4071b16b47de35
Christopher <christophergrey@berkeley.edu>
2020-12-05 14:45:14 -0800

    commiting to pull from main

--- pages/admin/invite/userAccountPage.tsx
+++ pages/admin/invite/userAccountPage.tsx
@@ -1,10 +1,46 @@
 import DashboardLayout from "components/DashboardLayout";
 // import Combobox from "components/Combobox";
-import React from "react";
+import React, { useState } from "react";
 // import { User } from "@prisma/client";
+import { useRouter } from "next/router";
+import { DeleteUserDTO } from "pages/api/admin/users/delete";
+import Combobox from "components/Combobox";
+import { User } from "@prisma/client";
 import Button from "../../../components/Button";
 
-const userAccountPage: React.FunctionComponent = () => {
+const userAccountPage: React.FunctionComponent<UserRequest> = ({
+  // firstName,
+  // lastName,
+  // email,
+  // phoneNumber,
+  // Role,
+  id,
+  onDelete,
+}) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [selectedPlayers, setSelectedPlayers] = useState<User[]>([]);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  // const [roleChosen] = useState("");
+  const deleteUser = async (): Promise<void> => {
+    try {
+      const response = await fetch("/api/admin/users/delete", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({
+          id,
+        } as DeleteUserDTO),
+      });
+      if (!response.ok) {
+        throw await response.json();
+      }
+      onDelete();
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  };
   return (
     <DashboardLayout>
       <div className="flex-col mx-16 mt-14">
@@ -20,22 +56,26 @@ const userAccountPage: React.FunctionComponent = () => {
         <p className="text-2xl font-semibold mb-10">Attached Note</p>
 
         <div>
+          <p className="text-2xl font-semibold"> Role Information </p>
           <p className="text-1xl font-semibold">Linked Players</p>
-          <p className="mb-10">
+          <p>
             Donors will have access to extended profiles of players they are
             sponsoring, including Engagement Scores, Academics, and Physical
             Health information.
           </p>
-          {/* <Combobox
-            selectedPlayers={selectedPlayers}
-            setSelectedPlayers={setSelectedPlayers}
-          /> */}
+          <p className="mb-20">
+            <Combobox
+              selectedPlayers={selectedPlayers}
+              setSelectedPlayers={setSelectedPlayers}
+              // role={roleChosen}
+            />
+          </p>
         </div>
         <div className="flex space-x-4 mb-10">
           <div>
             <Button
               className="bg-red-200 hover:bg-red-200 text-danger font-bold py-2 px-4 rounded"
-              // onClick={}
+              onClick={deleteUser}
             >
               Decline
             </Button>
@@ -54,7 +94,9 @@ const userAccountPage: React.FunctionComponent = () => {
         <div className="mb-10">
           <Button
             className="bg-white hover:bg-white text-blue font-bold py-2 px-4 rounded border-solid border-4 border-blue"
-            // onClick={}
+            onClick={() => {
+              router.push("./");
+            }}
           >
             Back
           </Button>
@@ -62,5 +104,14 @@ const userAccountPage: React.FunctionComponent = () => {
       </div>
     </DashboardLayout>
   );
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface UserRequest {
+    name: string;
+    email: string;
+    phoneNumber: string;
+    id: number;
+    onDelete: () => void;
+    onAccept: () => void;
+  }
 };
 export default userAccountPage;
