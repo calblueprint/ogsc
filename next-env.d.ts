50326126adca47a20f169bd6c76aae7aae261224
sydbui <sydbui@berkeley.edu>
2021-03-28 22:06:47 -0700

    add global to next-end.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient;
+    }
+  }
+}
 /// <reference types="next" />
 /// <reference types="next/types/global" />
