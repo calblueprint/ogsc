a8e57b55e2d4c42b19a92ae35da6e03534bb19a1
Joy <joydong@berkeley.edu>
2021-04-08 22:40:25 -0700

    rachel comments

--- pages/users/signUp/index.tsx
+++ pages/users/signUp/index.tsx
@@ -6,6 +6,7 @@ import UserSignUpFormField from "components/UserSignUpFormField";
 import { UserRoleType } from "interfaces";
 import Joi from "lib/validate";
 import { StateMachineProvider, useStateMachine } from "little-state-machine";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { UserDTO } from "pages/api/admin/users/readOneEmail";
 import React, { useState } from "react";
@@ -159,28 +160,34 @@ const UserSignUpPageOne: React.FC = () => {
                 name="password"
                 error={errors.password?.message}
               >
-                <input
-                  type={revealPassword ? "text" : "password"}
-                  className="input input-full"
-                  name="password"
-                  placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
-                  ref={register}
-                  defaultValue={state.userData.password}
-                />
-                <button
-                  className="text-sm text-gray-500"
-                  type="button"
-                  onClick={togglePassword}
-                >
-                  {revealPassword ? "Hide password" : "Show password"}
-                </button>
+                <div className="flex flex-row space-x-4">
+                  <input
+                    type={revealPassword ? "text" : "password"}
+                    className="input input-full"
+                    name="password"
+                    placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
+                    ref={register}
+                    defaultValue={state.userData.password}
+                  />
+                  <button
+                    className="text-sm text-gray-500"
+                    type="button"
+                    onClick={togglePassword}
+                  >
+                    {revealPassword ? "Hide password" : "Show password"}
+                  </button>
+                </div>
               </UserSignUpFormField>
               <div className="flex mt-24 mb-32 justify-between align-middle">
-                {/* TODO: link user login page */}
-                <p className="text-base">
-                  Already have an account?{" "}
-                  <b className="text-blue">Login here.</b>
-                </p>
+                <div className="flex flex-row">
+                  <p className="text-gray-600">Already have an account? </p>{" "}
+                  <Link href="/api/auth/signin">
+                    <a className="ml-1 text-blue font-semibold hover:underline">
+                      Login here.
+                    </a>
+                  </Link>
+                </div>
+
                 <div className="mb-2 flex ">
                   <Button
                     className="bg-blue text-sm px-5 py-2 text-white tracking-wide rounded-md"
