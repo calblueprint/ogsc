71d09666dcf3b2dcfbfdde01d31fd7a04d2920e6
Ethan Lee <hello@ethanl.ee>
2021-05-04 00:22:11 -0700

    Add production templates map, update fork sync to OaklandGenesis/ogsc

--- lib/notify/sendgrid.ts
+++ lib/notify/sendgrid.ts
@@ -9,10 +9,16 @@ import {
 export default class SendgridNotifier implements Notifier {
   id = "SENDGRID";
 
-  templates: Record<NotificationType, string> = {
+  stagingTemplates: Record<NotificationType, string> = {
     [NotificationType.ForgotPassword]: "d-9e3ecf30ca4846a68e1e879db5105974",
     [NotificationType.SignUpInvitation]: "d-3ad525e2c11840e59dce1a51d1d89004",
     [NotificationType.RequestAccepted]: "d-d3d3822022b548589003f1a005227616",
+  };
+
+  productionTemplates: Record<NotificationType, string> = {
+    [NotificationType.ForgotPassword]: "d-3ab72f8086884b51aa1e80e8e51b81bc",
+    [NotificationType.SignUpInvitation]: "d-86dc53c3c7a9457588b63e5491c7b13f",
+    [NotificationType.RequestAccepted]: "d-9d5cead29a7444198173ee3c74b39fc9",
   };
 
   async setup(): Promise<void> {
@@ -30,8 +36,13 @@ export default class SendgridNotifier implements Notifier {
   ): Promise<NotificationResult> {
     await sendgrid.send({
       to: meta.email,
-      from: "ogsc@calblueprint.org",
-      templateId: this.templates[type],
+      from:
+        process.env.VERCEL_ENV === "production"
+          ? "youth@oaklandgenesis.org"
+          : "ogsc@calblueprint.org",
+      templateId: (process.env.VERCEL_ENV === "production"
+        ? this.productionTemplates
+        : this.stagingTemplates)[type],
       dynamicTemplateData: meta,
     });
 
